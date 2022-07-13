5a78777520e9f46cf8d71d7726644147608e64ce
KasunUmesh <kasunumesh38@gmail.com>
2022-07-14 01:55:20 +0530

    Update Project

---
+++ Car_Rental_Back_End/src/main/java/lk/ijse/car_rental_system/entity/Customer.java
@@ -0,0 +1,26 @@
+package lk.ijse.car_rental_system.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class Customer {
+
+    private String userName;
+    private String address;
+    @Id
+    private String nic;
+    private String driverlicense;
+    private String email;
+    private String contact;
+    private String password;
+}
---
+++ Car_Rental_Back_End/src/main/java/lk/ijse/car_rental_system/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,4 @@
+package lk.ijse.car_rental_system.service.impl;
+
+public class CustomerServiceImpl {
+}
